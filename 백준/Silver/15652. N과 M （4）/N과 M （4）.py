6e10afa391ce80e7c5911de514edf29884f057c0
HongRok Lee <95682287+efforter@users.noreply.github.com>
2024-09-27 12:07:37 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def DFS(x):
+    if len(seq)==m:
+        print(*seq)
+        return
+    
+    for i in range(x,n+1):
+        seq.append(i)
+        DFS(i)
+        seq.pop()
+
+n,m = map(int,input().split())
+seq = []
+
+DFS(1)
